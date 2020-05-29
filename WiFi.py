1092aa59d16006e3405b407df80c4fcaafa9eb8a
Milan Ventil Toman <milan.v.toman@gmail.com>
2020-05-29 13:14:16 +0200

    First commit for the ESP8266 wifi davis logger

---
+++ WiFi.py
@@ -0,0 +1,105 @@
+import network
+import utime
+import machine
+
+_DEBUG = False
+
+
+class NetSet(object):
+
+    def __init__(self, wifi_type):
+        self.sta = network.WLAN(network.STA_IF)
+        self.ap = network.WLAN(network.AP_IF)
+        if wifi_type == 'infra':
+            if _DEBUG:
+                print("Disabling AP")
+            self.ap.active(False)
+            if _DEBUG:
+                print("Activating INFRA")
+            self.sta.active(True)
+            self.sta.isconnected() # False, it should be # Comments by Yoda
+            self._SSID = None
+            self._PASS = None
+            self._TIMEOUT = None
+        elif wifi_type == 'ap':
+            if _DEBUG:
+                print("Disabling INFRA")
+            self.ap.active(True)
+            if _DEBUG:
+                print("Activating AP")
+            self.sta.active(False)
+            self.sta.isconnected() # False, it should be # Comments by Yoda
+            self._SSID = None
+            self._PASS = None
+            self._TIMEOUT = None
+
+    def connectInfraGo(self, _timeout):
+        if _DEBUG:
+            print("Connecting to infra")
+        self.sta.connect(self._SSID, self._PASS)
+        if _DEBUG:
+            print("Let's wait for the network to come up")
+        while not (self.sta.isconnected()):
+            if _timeout > 0:
+                print("Trying... {} more times".format(_timeout))
+                utime.sleep_ms(1001)
+                _timeout -= 1
+            else:
+                print("Out of retrys")
+                return False
+        network_config = self.sta.ifconfig()
+        return network_config
+
+    def connectInfra(self, _SSID, _PASS, _TIMEOUT):
+        self._SSID = _SSID
+        self._PASS = _PASS
+        try:
+            net_conf_result = self.connectInfraGo(_TIMEOUT)
+            utime.sleep_ms(100)
+            if net_conf_result:
+                return {
+                    'ip': net_conf_result[0],
+                    'mask': net_conf_result[1],
+                    'gw': net_conf_result[2],
+                    'dns': net_conf_result[3]                    }
+            else:
+                return False
+        except Exception as e:
+            print("ERROR: Network configuration failed with: {}".format(e))
+            return False
+
+    def connectAp(self):
+        # TBI
+        pass
+
+    def readNetworkConfig(self):
+        self.config_dict = {}
+        try:
+            with open('inet.conf', 'r') as conf_handler:
+                for item in conf_handler.readlines():
+                    option = item.split("=")[0].strip()
+                    if len(item.split("=")) == 2 and '#' not in option:
+                        value = item.split("=")[1].strip()
+                        self.config_dict.update({option: value})
+                    else:
+                        if _DEBUG:
+                            if '#' in option:
+                                print(b"COMMENT in config")
+                            else:
+                                print(b"WARNING: Fucked up option, make it better")
+        except Exception as e:
+            if _DEBUG:
+                print("WARNING: Errors in INFRA config, still going for AP")
+            return False
+        if _DEBUG:
+            print("CONFIG DICT: {}".format(self.config_dict))
+        self._SSID = self.config_dict['_SSID']
+        self._PASS = self.config_dict['_PASS']
+        self._TIMEOUT = int(self.config_dict['_TIMEOUT'])
+        self._INFLUX_HOST = self.config_dict['_INFLUX_HOST']
+        self._INFLUX_PORT = self.config_dict['_INFLUX_PORT']
+        self._INFLUX_USER = self.config_dict['_INFLUX_USER']
+        self._INFLUX_PASS = self.config_dict['_INFLUX_PASS']
+        self._INF_DB_WEATHER = self.config_dict['_INF_DB_WEATHER']
+        self._INF_DB_STATUS = self.config_dict['_INF_DB_STATUS']
+        self._INF_DB_RAW = self.config_dict['_INF_DB_RAW']
---
+++ davis_decode.py
@@ -0,0 +1,223 @@
+import urequests
+
+
+_DEBUG = False
+
+
+def send_to_influx(host, port, db, user, password, davis_unit_id, wind, measurement, name, value, tags):
+    post = "http://{}:{}/write?db={}".format(host, port, db)
+    if _DEBUG:
+        print("SENDING TO: {}".format(post))
+    if measurement is False:
+        return (False, "ERROR measurement set False")
+    if measurement is None:
+        data = "wind,type=speed,davis_id={_davis_id} value={_speed}\n wind,type=direction,davis_id={_davis_id} value={_direction}".format(
+            _davis_id = davis_unit_id,
+            _speed=wind['speed'],
+            _direction=wind['direction'])
+        if _DEBUG:
+            print("SEND WIND only: {}")
+    else:
+        for tag in tags.keys():
+            measurement = "{},{}={}".format(measurement, tag, tags[tag])
+        data = "{_measure},davis_id={_davis_id} {_name}={_value}\n wind,type=speed,davis_id={_davis_id} value={_speed}\n wind,type=direction,davis_id={_davis_id} value={_direction}".format(
+            _measure=measurement,
+            _name=name,
+            _value=value,
+            _davis_id = davis_unit_id,
+            _speed=wind['speed'],
+            _direction=wind['direction'])
+    if _DEBUG:
+        print("POST_DATA: {}".format(data))
+    try:
+        return (True, urequests.post(post, data=data))
+    except Exception as e:
+        return (False, "ERROR sending data to influx: {}".format(e))
+
+
+def reverseBits(data):
+    data = "{:08b}".format(data)
+    z = ""
+    for i in range(len(data),0,-1):
+        z = z + (data[i-1])
+    return int(z, 2)
+
+
+class davisDecoder(object):
+    def __init__(self, weather_db, stat_db, raw_db):
+        __name__ = u'Davis value decoder class'
+        self.weather_influx_db = weather_db
+        self.stat_influx_db = stat_db
+        self.raw_influx_db = raw_db
+
+    def byte_split(self, data):
+        msb = data >> 4
+        lsb = data & 0b00001111
+        result = {"MSB": msb, "LSB": lsb}
+        return result
+
+    def davis_id(self, header):
+        self.davis_packet_id = 0
+        self.battery_low = 0
+        self.unit_id = 0
+        bin_header = self.byte_split(header)
+        self.unit_id = bin_header['LSB'] & 0b0111
+        self.battery_low = bin_header['LSB'] >> 3
+        self.davis_packet_id = bin_header['MSB']
+        result = {"davis_id": self.unit_id,
+                  "packet_id": self.davis_packet_id,
+                  "bat_low": self.battery_low}
+        return result
+
+    def decode_wind(self, databytes):
+        # wind speed in mph, i suppose. Let's convert it
+        wind_speed = round(float(databytes['windspeed'] * 1.60934), 1)
+        wind_direction_factor = round(float(360)/float(255), 1)
+        wind_direction = databytes['winddir']
+        wind_direction = float(wind_direction) * wind_direction_factor
+        result = {"speed": wind_speed, "direction": wind_direction}
+        return result
+
+    def decode_temp(self, temp):
+        temp_f = (float(temp)) / float(160) # in Fahrenheit
+        temp_c = round((temp_f - 32) * float(5)/float(9), 1)
+        result = {"celsius": temp_c, "fahrenheit": temp_f}
+        return result
+
+    def decode_humidity(self, hum):
+        pass
+
+    def supercap_decode(self, byte2, byte3):
+        cap = (byte2 << 2) + (byte3 >> 6)
+        result = float(cap / 100.00)
+        return result
+
+    def solarvolt_decode(self, byte2, byte3):
+        solar = (byte2 << 1) + (byte3 >> 7)
+        result = float(solar)
+        return result
+
+    def rain_decode(self, rain):
+        result = float(rain & 0x7F)
+        return result
+
+    def rainrate_decode(self, byte2, byte3):
+        # if byte3(b2 here) is 0xFF, or 255, there is no rain
+        #print("b2:{} b3:{} = result:{}".format(byte2, byte3, byte2 + (byte3 >> 4 << 8)))
+        if byte2 == 255:
+            rainstate = 0
+            rainrate = 0
+        elif byte2 == 254:
+            rainstate = 1
+            rainrate = 0.2
+        else:
+            rainstate = 2
+            if byte3 > 4:
+                rainrate = 720 / ((byte3 >> 4 << 8) + byte2)
+            else:
+                rainrate = 0
+        result = {"state": float(rainstate), "rate": float(rainrate)}
+        #print(result)
+        return result
+
+    def DecodePacket(self, packet):
+        # By default and most of the time, write to weather
+        self.write_influx_db = self.weather_influx_db
+        # Set all to None
+        self.wind = False
+        self.measurement = False
+        self.name = False
+        self.value = False
+        self.tags = False
+        self.wind = self.decode_wind(
+            {"windspeed": packet[1], "winddir": packet[2]})
+        if self.davis_packet_id == 2:
+            # SuperCap charge 0x2
+            if _DEBUG:
+                print('SCAP:')
+            supercap = self.supercap_decode(
+                packet[3], packet[4]
+            )
+            if _DEBUG:
+                print("{}".format(supercap))
+            self.write_influx_db = self.stat_influx_db
+            self.measurement = 'iss'
+            self.name = 'voltage'
+            self.tags = {'type': 'capacitor'}
+            self.value = supercap
+        elif self.davis_packet_id == 3:
+            # No fucking idea 0x3
+            # {'hop':1,'h':48,'b0':6,'b1':237,'b2':255,'b3':195,'b4':135,'b5':50,'b6':110,'b7':255,'b8':255,'b9':179,'rssi':45,'lqi':0,'nxt':64,'cnt':163}
+            self.measurement = None
+            self.name = None
+            self.tags = None
+            self.value = None
+
+        elif self.davis_packet_id == 5:
+            # Rainrate 0x5
+            rainrate_dict = self.rainrate_decode(
+                packet[3],
+                packet[4])
+            if _DEBUG:
+                print("RAINRATE: {}".format(rainrate_dict))
+            self.measurement = 'rain'
+            self.name = 'value'
+            self.tags = {'type': 'rainrate'}
+            self.value = rainrate_dict['rate']
+        elif self.davis_packet_id == 6:
+            # Sun Irradiation 0x6 (NOT ON vantage Vue)
+            pass
+        elif self.davis_packet_id == 7:
+            # Super Cap voltage 0x7
+            solarvolt = self.solarvolt_decode(
+                packet[3], packet[4]
+            )
+            if _DEBUG:
+                print("SOLV {}".format(solarvolt))
+            self.write_influx_db = self.stat_influx_db
+            self.measurement = 'iss'
+            self.name = 'voltage'
+            self.tags = {'type': 'solar'}
+            self.value = solarvolt
+        elif self.davis_packet_id == 8:
+            # Temperature 0x8
+            raw_temp = (packet[3] << 8) + packet[4]
+            temp_dict = self.decode_temp(raw_temp)
+            temp = float(temp_dict['celsius'])
+            if _DEBUG:
+                print("TEMP: {}".format(temp))
+            self.measurement = 'temphumi'
+            self.name = 'temperature'
+            self.tags = {'type': 'external'}
+            self.value = temp
+        elif self.davis_packet_id == 9:
+            # Wind gusts 0x9
+            windgust = packet[3] * 1.60934
+            if _DEBUG:
+                print("WINDGUST: {}".format(windgust))
+            self.measurement = 'wind'
+            self.name = 'value'
+            self.tags = {'type': 'windgust'}
+            self.value = windgust
+        elif self.davis_packet_id == 10:
+            # Humidity 0xa
+            raw_humidity = (((packet[4] >> 4) & 0b0011) << 8) \
+                         + packet[3]
+            humidity = round(int(raw_humidity) / float(10), 1)
+            if _DEBUG:
+                print("HUMI: {}".format(humidity))
+            self.measurement = 'temphumi'
+            self.name = 'humidity'
+            self.tags = {'type': 'external'}
+            self.value = humidity
+        elif self.davis_packet_id == 14:
+            # Rain bucket tips 0xe
+            raw_rain = (packet[3]) + (packet[4] >> 7 << 8)
+            rain = self.rain_decode(raw_rain)
+            if _DEBUG:
+                print("RAINCOUNT: {}".format(rain))
+            self.measurement = 'rain'
+            self.name = 'value'
+            self.tags = {'type': 'rain_bucket_tips'}
+            self.value = rain
+
